a7dc71fdaaa6c7c4169eb086bebd3a6952e1187c
Igor Goryainov <sibuty132@gmail.com>
2017-03-13 01:26:16 +0400

    #9178 Добавил Splash экран с дополнительными задержками времени.

---
+++ app/src/main/java/ru/digitalwand/nonstopfit/ui/SplashActivity.java
@@ -0,0 +1,36 @@
+package ru.digitalwand.nonstopfit.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.digitalwand.nonstopfit.R;
+import ru.digitalwand.nonstopfit.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+  private static final long TIME_WAIT_STUB = 5000L;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    try {
+      Thread.sleep(TIME_WAIT_STUB);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    setContentView(R.layout.activity_splash);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    new Handler().postDelayed(this::afterSplash, TIME_WAIT_STUB);
+  }
+
+  private void afterSplash() {
+    startActivity(new Intent(this, LoginActivity.class));
+    finish();
+  }
+}
